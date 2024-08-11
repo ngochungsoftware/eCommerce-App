2dda57d3f12cc18641ed4a8ce944928d67c9e829
ngochungsoftware <hungit2301@gmail.com>
2024-08-11 13:59:09 +0700

    base project

---
+++ src/main/java/com/standard/demo/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.standard.demo.service;
+
+import com.standard.demo.entity.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
+
+    Category updateCategory(Category category, Long categoryId);
+}
