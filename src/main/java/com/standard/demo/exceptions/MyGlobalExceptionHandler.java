13ce22f9861170049e6422862e1c021630434d02
ngochungsoftware <hungit2301@gmail.com>
2024-08-13 18:06:51 +0700

    create exception

---
+++ src/main/java/com/standard/demo/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.standard.demo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class MyGlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        Map<String, String> response = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            response.put(fieldName,message);
+        });
+        return  new ResponseEntity<Map<String , String>>(response, HttpStatus.BAD_REQUEST);
+    }
+
+
+}
